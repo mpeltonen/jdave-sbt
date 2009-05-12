b1559aa5c18c0ba0021fa0ea970bdddf213ecb09
Mikko Peltonen <mikko.peltonen@netikka.fi>
2009-05-12 20:01:07 +0300

    Fix formatting

--- src/main/scala/jdave/sbt/JDaveSupport.scala
+++ src/main/scala/jdave/sbt/JDaveSupport.scala
@@ -3,10 +3,10 @@ package jdave.sbt
 import _root_.sbt._
 
 object JDaveFramework extends LazyTestFramework {
-	val name = "JDave"
+  val name = "JDave"
 
-	def testSuperClassName = "jdave.Specification"
-	def testSubClassType = ClassType.Class
+  def testSuperClassName = "jdave.Specification"
+  def testSubClassType = ClassType.Class
 
   def testRunnerClassName = "jdave.sbt.JDaveRunner"
 }
@@ -16,37 +16,37 @@ import java.lang.reflect.Method
 import _root_.jdave.{Specification, ExpectationFailedException}
 import _root_.jdave.runner.{ISpecVisitor, Context, Behavior, SpecRunner, IBehaviorResults}
 
-	def runTest(testClassName: String): Result.Value = {
-		val testClass = Class.forName(testClassName, true, testLoader).asSubclass(classOf[Specification[_]])
-		val specVisitor = new JDaveSpecVisitor
-		new SpecRunner().run(testClass, specVisitor)
-	 	specVisitor.testResult
-	}
+  def runTest(testClassName: String): Result.Value = {
+    val testClass = Class.forName(testClassName, true, testLoader).asSubclass(classOf[Specification[_]])
+    val specVisitor = new JDaveSpecVisitor
+    new SpecRunner().run(testClass, specVisitor)
+    specVisitor.testResult
+  }
 
-	private class JDaveSpecVisitor extends ISpecVisitor {
-		var testResult = Result.Passed
+  private class JDaveSpecVisitor extends ISpecVisitor {
+    var testResult = Result.Passed
 
-		def onContext(context: Context) = log.info("  " + context.getName)
+    def onContext(context: Context) = log.info("  " + context.getName)
 
     def onBehavior(behavior: Behavior) {
-			behavior.run(new IBehaviorResults {
-			  def expected(method: Method) = log.info("  + " + behavior.getName)
+      behavior.run(new IBehaviorResults {
+        def expected(method: Method) = log.info("  + " + behavior.getName)
 
         def unexpected(method: Method, e: ExpectationFailedException) {
-					log.error(" - " + behavior.getName + ": " + e.getMessage)
-					log.trace(e)
-					testResult = Result.Failed
-				}
+          log.error(" - " + behavior.getName + ": " + e.getMessage)
+          log.trace(e)
+          testResult = Result.Failed
+        }
 
         def error(method: Method, t: Throwable) {
-					log.error(" - " + behavior.getName + ": " + t.getMessage)
-					log.trace(t)
-					testResult = Result.Failed
-				}
-			})
-		}
+          log.error(" - " + behavior.getName + ": " + t.getMessage)
+          log.trace(t)
+          testResult = Result.Failed
+        }
+      })
+    }
 
-		def afterContext(context: Context) {}
-	}
+    def afterContext(context: Context) {}
+  }
 }
 
